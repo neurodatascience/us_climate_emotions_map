8e0b179230c40fab9ccb7376cbe1318232113449
Alyssa Dai <alyssa.ydai@gmail.com>
2024-04-30 13:45:48 -0400

    add variable to store data dictionaries

--- climate_emotions_map/data_loader.py
+++ climate_emotions_map/data_loader.py
@@ -1,20 +1,33 @@
+"""
+Load survey data and data dictionaries into dataframes.
+
+Example usage: After importing SURVEY_DATA in another script, the dataframe for the
+opinions_party.tsv data file can be accessed with SURVEY_DATA["opinions_party.tsv"].
+"""
+
 from pathlib import Path
 
 import pandas as pd
 
 
 def load_data_file(file: str) -> pd.DataFrame:
-    """
-    Load the data from one TSV file into a dataframe.
-    """
+    """Load a TSV data file into a dataframe."""
     return pd.read_csv(
         Path(__file__).parents[1] / "data" / "survey_results" / file, sep="\t"
     )
 
 
-def load_survey_data():
+def load_data_dictionary(file: str) -> pd.DataFrame:
+    """Load a data dictionary TSV into a dataframe."""
+    return pd.read_csv(
+        Path(__file__).parents[1] / "data" / "data_dictionaries" / file,
+        sep="\t",
+    )
+
+
+def load_survey_data() -> dict[str, pd.DataFrame]:
     """
-    Load all survey result TSV files into dataframes.
+    Load all survey result TSV files of interest.
     Climate impact data is excluded because it is not used in the dashboard.
     """
     data_files = [
@@ -34,4 +47,22 @@ def load_survey_data():
     return data_frames
 
 
+def load_data_dictionaries() -> dict[str, pd.DataFrame]:
+    """Load the data dictionaries for the survey data."""
+    data_files = [
+        "impacts_list.tsv",
+        "outcome_dictionary.tsv",
+        "question_dictionary.tsv",
+        "state_abbreviations.tsv",
+        "subquestion_dictionary.tsv",
+    ]
+
+    data_frames = {}
+    for file in data_files:
+        data_frames[file] = load_data_dictionary(file)
+
+    return data_frames
+
+
 SURVEY_DATA = load_survey_data()
+DATA_DICTIONARIES = load_data_dictionaries()
