71eb5eb45517aace01f5d98de264375c251bb8ad
Michelle Wang <tomichellewang@gmail.com>
2024-05-09 17:58:43 -0400

    update make_map to use custom GeoJSON state/cluster shapes

--- climate_emotions_map/make_map.py
+++ climate_emotions_map/make_map.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-from data_loader import DATA_DICTIONARIES, SURVEY_DATA
+from data_loader import DATA_DICTIONARIES, GEOJSON_OBJECTS, SURVEY_DATA
+
+survey_states = GEOJSON_OBJECTS["survey_states.json"]
 
 opinions_state = SURVEY_DATA["opinions_state.tsv"]
 
@@ -13,18 +15,19 @@ state_abbreviations = DATA_DICTIONARIES["state_abbreviations.tsv"]
 def get_state_abbrevs_in_long_format(state_abbreviations=state_abbreviations):
     # put the state abbreviations in a format where there's one row per state
     # i.e., flatten out the clusters
+    # but still keep the "state" column as the original state/cluster name
     states = []
+    single_states = []
     abbrevs = []
     for _, row in state_abbreviations.iterrows():
-        state_list = row["state"].split(", ")
-        states.extend(
-            state.split("(")[0].replace("(", "") for state in state_list
-        )
+        state_list = row["state"].split(" (")[0].split(", ")
+        single_states.extend(state_list)
         abbrev_list = row["state_abbreviated"].split(", ")
         abbrevs.extend(iter(abbrev_list))
+        states.extend([row["state"]] * len(state_list))
     state_abbrevs_long = pd.DataFrame(
-        data=np.array([states, abbrevs]).T,
-        columns=["state", "state_abbreviated"],
+        data=np.array([states, single_states, abbrevs]).T,
+        columns=["state", "single_state", "state_abbreviated"],
     )
     return state_abbrevs_long
 
@@ -192,5 +195,136 @@ def make_map():
     fig.show()
 
 
+def make_map2(
+    question: str,
+    sub_question: int,
+    outcome: str,
+    clicked_state: str,
+    opinions_or_impacts: str,  # TODO
+    number_of_impacts: int,  # TODO
+    opinion_colormap: str | None = None,
+    clicked_state_marker: dict | None = None,
+):
+    # constants
+    col_location = "state"
+    col_color = "percentage"
+
+    # default values
+    if clicked_state_marker is None:
+        clicked_state_marker = dict(line=dict(width=4, color="yellow"))
+
+    # get the state abbreviations in long format
+    # "state" (i.e. state or cluster), "single_state", "state_abbreviated"
+    state_abbrevs_long = get_state_abbrevs_in_long_format()
+
+    # get the question data and change percentage to be between 0 and 100
+    df_opinions_to_plot = opinions_state.loc[
+        (opinions_state["question"] == question)
+        & (opinions_state["sub_question"] == str(sub_question))
+        & (opinions_state["outcome"] == outcome)
+    ].copy(deep=True)
+    df_opinions_to_plot[col_color] *= 100
+
+    if len(df_opinions_to_plot) == 0:
+        raise RuntimeError(
+            f"No data found for question {question} ({type(question)})"
+            f", sub_question {sub_question} ({type(sub_question)})"
+            f", outcome {outcome} ({type(outcome)})"
+        )
+
+    # get minimum/maximum values for scaling the colormap
+    vmin = df_opinions_to_plot[col_color].min()
+    vmax = df_opinions_to_plot[col_color].max()
+
+    # initialize the figure
+    fig = go.Figure()
+
+    # plot the question data on a map
+    # do not show the hoverboxes here because for some reason they are not centered properly
+    fig.add_choropleth(
+        locations=df_opinions_to_plot[col_location],
+        geojson=survey_states,
+        z=df_opinions_to_plot[col_color],
+        zmin=vmin,
+        zmax=vmax,
+        colorscale=opinion_colormap,
+        colorbar_title=col_color.capitalize(),
+        name="main_map",
+        hoverinfo="none",  # no hoverbox but click events are still emitted (?)
+    )
+
+    # add outline for clicked state
+    df_opinions_to_plot_clicked = df_opinions_to_plot[
+        df_opinions_to_plot[col_location] == clicked_state
+    ]
+    fig.add_choropleth(
+        locations=df_opinions_to_plot_clicked[col_location],
+        geojson=survey_states,
+        z=df_opinions_to_plot_clicked[col_color],
+        zmin=vmin,
+        zmax=vmax,
+        colorscale=opinion_colormap,
+        hoverinfo="skip",
+        name="clicked_state",
+        marker=clicked_state_marker,
+        showscale=False,
+    )
+
+    # add hover information
+    df_hover_data = state_abbrevs_long.merge(
+        df_opinions_to_plot,
+        on=col_location,
+    )
+    fig.add_choropleth(
+        locations=df_hover_data["state_abbreviated"],
+        locationmode="USA-states",
+        customdata=df_hover_data[col_location],
+        z=df_hover_data[col_color],
+        marker=dict(opacity=0),
+        name="hover_info",
+        hovertemplate=f"%{{customdata}}<br>{col_color.capitalize()}: %{{z:.2f}}<extra></extra>",
+        showscale=False,
+    )
+
+    # add state abbreviation labels
+    fig.add_scattergeo(
+        locations=state_abbrevs_long["state_abbreviated"],
+        locationmode="USA-states",
+        text=state_abbrevs_long["state_abbreviated"],
+        mode="text",
+        hoverinfo="skip",
+        name="abbr_labels",
+    )
+
+    # do not show base map
+    fig.update_geos(visible=False)
+
+    # zoom in to the US
+    fig.update_layout(
+        geo_scope="usa",
+    )
+
+    return fig
+
+
 if __name__ == "__main__":
-    make_map()
+    # # old function
+    # make_map()
+
+    # pick a random state/cluster to highlight
+    states = state_abbreviations["state"].tolist()
+    clicked_state = np.random.choice(states)
+    # clicked_state = "Colorado, New Mexico (Cluster E)"  # example cluster
+    print(f"clicked_state: {clicked_state}")
+
+    fig = make_map2(
+        question="q2",
+        sub_question=1,
+        outcome="3+",
+        clicked_state=clicked_state,
+        opinions_or_impacts="opinions",
+        number_of_impacts=4,
+        opinion_colormap="Viridis",
+    )
+
+    fig.show()
