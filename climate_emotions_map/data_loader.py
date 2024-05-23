37debfa93917b9d07183ddcfc447fe2d684bf068
rmanaem <rmanaem@protonmail.ch>
2024-05-23 15:47:03 -0400

    Fixed typo

--- climate_emotions_map/data_loader.py
+++ climate_emotions_map/data_loader.py
@@ -73,4 +73,4 @@ def load_data_dictionaries() -> dict[str, pd.DataFrame]:
 
 SURVEY_DATA = load_survey_data()
 DATA_DICTIONARIES = load_data_dictionaries()
-NATIONAL_SAMPLE_SIZE = SURVEY_DATA["samplesizes_State.tsv"]["n"].sum()
+NATIONAL_SAMPLE_SIZE = SURVEY_DATA["samplesizes_state.tsv"]["n"].sum()
