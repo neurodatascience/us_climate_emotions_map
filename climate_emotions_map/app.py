8d19b42ef354c84a761b8300c3d4d8f28505796f
Alyssa Dai <alyssa.ydai@gmail.com>
2024-05-23 21:35:23 -0400

    disable state select/party stratify switch when the other is selected

--- climate_emotions_map/app.py
+++ climate_emotions_map/app.py
@@ -3,7 +3,8 @@
 import os
 
 import dash_mantine_components as dmc
-from dash import Dash
+from dash import Dash, Input, Output, ctx, no_update
+from dash.exceptions import PreventUpdate
 
 from .layout import construct_layout
 
@@ -18,6 +19,37 @@ app = Dash(
 app.layout = dmc.MantineProvider(construct_layout(), forceColorScheme="light")
 
 server = app.server
+
+
+@app.callback(
+    [
+        Output("state-select", "value"),
+        Output("state-select", "disabled"),
+        Output("party-stratify-switch", "checked"),
+        Output("party-stratify-switch", "disabled"),
+    ],
+    [
+        Input("party-stratify-switch", "checked"),
+        Input("state-select", "value"),
+    ],
+    prevent_initial_call=True,
+)
+def disable_state_select_and_party_switch_interaction(
+    is_party_stratify_checked, selected_state
+):
+    """
+    Disable the state dropdown when the party stratify switch is checked,
+    and disable the party stratify switch when a specific state is selected (i.e., not None).
+    """
+    if ctx.triggered_id == "party-stratify-switch":
+        # Deselect any state
+        return None, is_party_stratify_checked, no_update, no_update
+    if ctx.triggered_id == "state-select":
+        if selected_state is not None:
+            return no_update, no_update, False, True
+        return no_update, no_update, False, False
+    return PreventUpdate
+
 
 if __name__ == "__main__":
     app.run(debug=True)
