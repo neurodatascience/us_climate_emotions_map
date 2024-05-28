neurodatascience/us_climate_emotions_map

commit da5758724c061a6c0b539c0f6af0914aff931408

[FIX] set React18 via dash_renderer (#40)
* [FIX] set React18 via dash_renderer

- replace direct ENV var setting
  for dash mantine React version
  with the dash_renderer call

Fixes #39

* remove old imports

climate_emotions_map/app.py
@@ -1,9 +1,7 @@
 """Main file to run the Dash app."""
 
-import os
-
 import dash_mantine_components as dmc
-from dash import Dash, Input, Output, callback, ctx, no_update
+from dash import Dash, Input, Output, callback, ctx, , _dash_renderer, no_update
 
 from . import utility as utils
 from .data_loader import NATIONAL_SAMPLE_SIZE, SURVEY_DATA
@@ -14,7 +12,7 @@ from .make_stacked_bar_plots import make_stacked_bar
 from .utility import DEFAULT_QUESTION  # IMPACT_COLORMAP,; OPINION_COLORMAP,
 
 # Currently needed by DMC, https://www.dash-mantine-components.com/getting-started#simple-usage
-os.environ["REACT_VERSION"] = "18.2.0"
+_dash_renderer._set_react_version("18.2.0")
 
 app = Dash(
     __name__,

commit 91df37b7b039dc436f8c73df470f197c2d6386c6

[FIX] correct broken imports from PR #40

climate_emotions_map/app.py
@@ -1,7 +1,7 @@
 """Main file to run the Dash app."""
 
 import dash_mantine_components as dmc
-from dash import Dash, Input, Output, callback, ctx, , _dash_renderer, no_update
+from dash import Dash, Input, Output, _dash_renderer, callback, ctx, no_update
 
 from . import utility as utils
 from .data_loader import NATIONAL_SAMPLE_SIZE, SURVEY_DATA