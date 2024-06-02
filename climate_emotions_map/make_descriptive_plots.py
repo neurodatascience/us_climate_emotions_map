bee4e05e4e33f03db3e2355992c4337f779087a9
Michelle Wang <tomichellewang@gmail.com>
2024-06-02 16:29:58 -0400

    try stacked bar plots for demographics

--- climate_emotions_map/make_descriptive_plots.py
+++ climate_emotions_map/make_descriptive_plots.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+from functools import partial
+
 import pandas as pd
 import plotly.graph_objects as go
+from data_loader import DATA_DICTIONARIES, SURVEY_DATA
 from plotly.subplots import make_subplots
 
-from .data_loader import SURVEY_DATA
-
+DEMOGRAPHICS_DICTIONARY = DATA_DICTIONARIES["demographics_dictionary.tsv"]
 SAMPLEDESC_WHOLESAMPLE: pd.DataFrame = SURVEY_DATA[
     "sampledesc_wholesample.tsv"
 ]
@@ -16,7 +18,7 @@ COL_N = "n"
 COL_PERCENTAGE = "percentage"
 
 # special handling of 7 weather impacts
-IMPACTS_LABEL = "impacts"
+IMPACTS_LABEL = "impact"
 IMPACT_VARIABLES = sorted(
     ["drought", "flood", "heat", "hurricane", "smoke", "tornado", "wildfire"]
 )
@@ -113,16 +115,16 @@ CATEGORY_ORDERS.update(
 
 SUBPLOT_POSITIONS = {
     "age": (1, 1),
-    "sex": (1, 3),
-    "party": (1, 5),
-    "race": (2, 1),
-    "ethnicity": (2, 4),
-    "student": (3, 1),
-    "employed": (3, 4),
-    "location": (4, 1),
-    "hh_origin": (4, 4),
-    IMPACTS_LABEL: (5, 1),
-    "q2": (6, 1),
+    "sex": (1, 1),
+    "race": (1, 1),
+    "ethnicity": (1, 1),
+    "party": (1, 1),
+    "student": (1, 1),
+    "employed": (1, 1),
+    "location": (1, 1),
+    "hh_origin": (1, 1),
+    IMPACTS_LABEL: (4, 1),
+    "q2": (5, 1),
 }
 
 
@@ -141,26 +143,24 @@ def get_categories_dict(df: pd.DataFrame) -> dict:
 
 
 def get_demographic_variable_to_display(demographic_variable: str):
-    if demographic_variable == "hh_origin":
-        demographic_variable = "SES of household of origin"
-    elif demographic_variable == "q2":
-        demographic_variable = (
-            "How sure are you that climate change is or is not happening?"
-        )
-    return demographic_variable.capitalize()
+    return (
+        DEMOGRAPHICS_DICTIONARY.set_index(COL_DEMOGRAPHIC_VARIABLE)
+        .loc[demographic_variable]
+        .item()
+    )
 
 
 def get_category_to_display(category: str):
     return category.capitalize()
 
 
-def make_descriptive_plot(
-    df: pd.DataFrame, demographic_variable: str, **kwargs
+def make_descriptive_plot_traces(
+    df: pd.DataFrame,
+    demographic_variable: str,
+    reverse=True,
 ) -> go.Bar:
     """
     Make a single plot for a descriptive demographic variable.
-
-    Extra kwargs are passed to plotly.express.bar()
 
     Parameters
     ----------
@@ -179,25 +179,64 @@ def make_descriptive_plot(
     df[COL_CATEGORY] = pd.Categorical(
         df[COL_CATEGORY], categories=CATEGORY_ORDERS[demographic_variable]
     )
-    df = df.sort_values(COL_CATEGORY)
+    df = df.sort_values(COL_CATEGORY)  # , ascending=not reverse)
 
-    # make the bar plot
-    bar_plot = go.Bar(
-        x=df[COL_CATEGORY].apply(get_category_to_display),
-        y=df[COL_N],
-        customdata=df[COL_PERCENTAGE] * 100,
+    bar_plot_trace_without_text = partial(
+        go.Bar,
+        x=df[COL_PERCENTAGE] * 100,
+        y=(
+            [get_demographic_variable_to_display(demographic_variable)]
+            * len(df[COL_CATEGORY].apply(get_category_to_display))
+        ),
+        hoverinfo="none",
+        customdata=list(
+            zip(
+                df[COL_N],
+                df[COL_CATEGORY].apply(get_category_to_display),
+            )
+        ),
+        orientation="h",
+        # textposition="inside",
         # text=[
         #     f"{n} ({percentage*100:.1f}%)"
         #     for n, percentage in zip(df[COL_N], df[COL_PERCENTAGE])
         # ],
-        hovertemplate=(
-            f"<b>{get_demographic_variable_to_display(demographic_variable)}</b>"
-            "<br>%{x}: %{y} (%{customdata:.1f}%)"
-            "<extra></extra>"
-        ),
+        # hovertemplate=(
+        #     f"<b>{get_demographic_variable_to_display(demographic_variable)}</b>"
+        #     "<br>%{x}: %{y} (%{customdata:.1f}%)"
+        #     "<extra></extra>"
+        # ),
         name=demographic_variable,
+        offsetgroup=0,
     )
-    return bar_plot
+
+    traces = []
+    # make the bar plot
+    traces.append(
+        bar_plot_trace_without_text(
+            textposition="inside",
+            text=[
+                f"{get_category_to_display(category)}: {percentage*100:.1f}% ({n})"
+                for n, percentage, category in zip(
+                    df[COL_N], df[COL_PERCENTAGE], df[COL_CATEGORY]
+                )
+            ],
+            hovertemplate=(
+                # f"<b>{get_demographic_variable_to_display(demographic_variable)}</b>"
+                "<b>%{customdata[1]}</b>: %{x:.2f}% (%{customdata[0]})"
+                "<extra></extra>"
+            ),
+        ),
+    )
+
+    # traces.append(
+    #     bar_plot_trace_without_text(
+    #         textposition="outside",
+    #         text=df[COL_CATEGORY].apply(get_category_to_display),
+    #         marker_color="rgba(0,0,0,0)",
+    #     )
+    # )
+    return traces
 
 
 def make_impact_plot_traces(df: pd.DataFrame):
@@ -209,7 +248,7 @@ def make_impact_plot_traces(df: pd.DataFrame):
     )
     data_impact = data_impact.sort_values(COL_DEMOGRAPHIC_VARIABLE)
     traces = []
-    for category in ["No", "Yes"]:
+    for category in ["Yes"]:
         data_category = data_impact.loc[data_impact[COL_CATEGORY] == category]
         traces.append(
             go.Bar(
@@ -223,6 +262,7 @@ def make_impact_plot_traces(df: pd.DataFrame):
                     f"<br>{category}: %{{y}} (%{{customdata:.1f}}%)"
                     "<extra></extra>"
                 ),
+                constraintext="outside",
                 name=category,
             )
         )
@@ -239,55 +279,85 @@ def make_descriptive_plots(state: str | None = None) -> go.Figure:
 
     # initialize figure
     fig = make_subplots(
-        rows=6,
-        cols=6,
+        rows=5,
+        cols=1,
         specs=[
-            [{"colspan": 2}, None, {"colspan": 2}, None, {"colspan": 2}, None],
-            [{"colspan": 3}, None, None, {"colspan": 3}, None, None],
-            [{"colspan": 3}, None, None, {"colspan": 3}, None, None],
-            [{"colspan": 3}, None, None, {"colspan": 3}, None, None],
-            [{"colspan": 6}, None, None, None, None, None],
-            [{"colspan": 6}, None, None, None, None, None],
+            [{"rowspan": 3}],
+            [None],
+            [None],
+            [{"rowspan": 1}],
+            [{"rowspan": 1}],
+            # [{"colspan": 2}, None, {"colspan": 2}, None, {"colspan": 2}, None],
+            # [{"colspan": 3}, None, None, {"colspan": 3}, None, None],
+            # [{"colspan": 3}, None, None, {"colspan": 3}, None, None],
+            # [{"colspan": 3}, None, None, {"colspan": 3}, None, None],
+            # [{"colspan": 6}, None, None, None, None, None],
+            # [{"colspan": 6}, None, None, None, None, None],
         ],
         subplot_titles=[
-            get_demographic_variable_to_display(demographic_variable)
-            for demographic_variable in SUBPLOT_POSITIONS.keys()
+            "Demographic information",
+            get_demographic_variable_to_display(IMPACTS_LABEL),
+            get_demographic_variable_to_display("q2"),
         ],
+        # subplot_titles=[
+        #     get_demographic_variable_to_display(demographic_variable)
+        #     for demographic_variable in SUBPLOT_POSITIONS.keys()
+        # ],
     )
 
     # add plots
-    for demographic_variable in SUBPLOT_POSITIONS.keys():
+    for demographic_variable in reversed(SUBPLOT_POSITIONS.keys()):
 
         row, col = SUBPLOT_POSITIONS[demographic_variable]
 
         if demographic_variable == IMPACTS_LABEL:
-            for trace in make_impact_plot_traces(data):
-                fig.add_trace(trace, row=row, col=col)
+            traces = make_impact_plot_traces(data)
+            horizontal = False
         else:
-            fig.add_trace(
-                make_descriptive_plot(data, demographic_variable),
+            traces = make_descriptive_plot_traces(data, demographic_variable)
+            horizontal = True
+
+        for trace in traces:
+            fig.add_trace(trace, row=row, col=col)
+
+        if horizontal:
+            fig.update_yaxes(
+                # ticklabelposition="inside",
+                # tickfont={"color": "rgba(0,0,0,0)"},
                 row=row,
                 col=col,
             )
+            fig.update_xaxes(range=[0, 100], row=row, col=col)
+
+    # fig.update_yaxes(
+    #     tickmode="array",
+    #     ticklabelposition="inside",
+    #     tickvals=y,
+    #     ticktext=y,
+    #     row=row,
+    #     col=col,
+    # )
 
     # turn off legend
     fig.update_layout(showlegend=False)
 
+    fig.update_layout(margin={"l": 30, "r": 30, "t": 30, "b": 30})
+
     return fig
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     # # whole sample
-#     # fig = make_descriptive_plots()
+    # whole sample
+    fig = make_descriptive_plots()
 
-#     # specific state
-#     import numpy as np
+    # # specific state
+    # import numpy as np
 
-#     states: list = SAMPLEDESC_STATE["state"].unique().tolist()
-#     states.append(None)
-#     state = np.random.choice(states)
-#     print(f"state: {state}")
-#     fig = make_descriptive_plots(state=state)
+    # states: list = SAMPLEDESC_STATE["state"].unique().tolist()
+    # states.append(None)
+    # state = np.random.choice(states)
+    # print(f"state: {state}")
+    # fig = make_descriptive_plots(state=state)
 
-#     fig.show()
+    fig.show()
